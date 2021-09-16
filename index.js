f5fd26197e7d266275e95457cfa62e1c78224551
LauraAnneB <88866166+LauraAnneB@users.noreply.github.com>
2021-09-16 15:47:39 +0200

    Added Flags

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function playGame () {
   if (randomNumber1 === randomNumber2) {
     document.querySelector("h1").innerHTML = "Draw!";
   } else if (randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 1 wins!";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
   } else {
-    document.querySelector("h1").innerHTML = "Player 2 wins!";
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
   }
 }
